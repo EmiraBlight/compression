4067af07f115507c64ab3f004f68d8ab0b7cc997
Sammy <Sammy@dev-crypt.com>
2026-03-05 07:35:41 -0800

    Added database capability to create bitmap and write to file

--- compress.py
+++ compress.py
@@ -1,23 +1,5 @@
-from io import TextIOWrapper
+from database import db
 
-from row import entry
-
-
-def readFile(name: str) -> list[entry]:
-    """returns a list of lists that consist a str, int bool each
-    Ex "bird,84,False" -> ["bird", 84, False]
-    this will make logic easier later down the line"""
-    f: TextIOWrapper = open(name, "r")
-    return [entry(line.split(",")) for line in f.read().splitlines()]
-
-
-def toFile(arr: list[entry], fileName: str) -> None:
-    f = open(fileName, "w")
-    for i in arr:
-        f.write(i.toBin() + "\n")
-    f.close()
-
-
-rows = readFile("data/animals_small.txt")
-
-toFile(rows, "animals_small")
+d = db("data/animals_small.txt")
+print(d)
+d.toBin("test")
--- database.py
+++ database.py
@@ -1,7 +1,25 @@
 from row import entry
 
 
-class database:
+class db:
     rows: list[entry]
     bitmap: list[str]
-    compressionType: str
+
+    def __init__(self, file: str):
+        f = open(file, "r")
+        self.rows = [entry(line.split(",")) for line in f.read().splitlines()]
+        self.bitmap = [i.toBin() for i in self.rows]
+        f.close()
+
+    def __repr__(self) -> str:
+        result = ""
+        for i in range(len(self.rows)):
+            result += f"Entry:{self.rows[i]}\nbin:{self.bitmap[i]}\n"
+            result += "-" * 50 + "\n"
+        return result
+
+    def toBin(self, file):
+        f = open(file, "w")
+        for i in self.bitmap:
+            f.write(i + "\n")
+        f.close()
