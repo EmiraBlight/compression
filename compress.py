07d06522c429bf252987579de3b369a9d24f5faf
Sammy <sam.thorne00@gmail.com>
2026-03-06 16:23:25 -0800

    now does WAH compression, in process of debugging

--- compress.py
+++ compress.py
@@ -2,7 +2,5 @@ from database import db
 
 d = db("data/animals_small.txt")
 # print(d)
-d.toBin("test")
 d.getOrder()
-d.getWords()
-print(d.words)
+d.wah()
--- database.py
+++ database.py
@@ -8,6 +8,10 @@ class db:
     wordSize: int = 8
     words: list[str] = []
 
+    @staticmethod
+    def isRun(s: str) -> bool:
+        return len(set(s)) == 1
+
     def __init__(self, file: str):
         f = open(file, "r")
         self.rows = [entry(line.split(",")) for line in f.read().splitlines()]
@@ -39,5 +43,44 @@ class db:
     def getWords(self) -> None:
         self.words = [
             str(self.compressOrder[i : i + self.wordSize - 1])
-            for i in range(0, len(self.bitmap), self.wordSize - 1)
+            for i in range(0, len(self.compressOrder), self.wordSize - 1)
         ]
+
+    def wah(self):
+        self.getWords()
+        result: str = ""
+        in_run: bool = False
+        run_len: int = 0
+        run_type: None | str = None
+
+        for string in self.words:
+            print(string)
+            if in_run and run_type:
+                if self.isRun(string):
+                    if string[0] == run_type:
+                        run_len += 1
+
+                        if len(bin(run_len)) > self.wordSize:
+                            result += (
+                                "1" + run_type + f"{run_len:0{self.wordSize - 2}b}"
+                            )
+                            run_len = 1
+                    else:
+                        result += "1" + run_type + f"{run_len:0{self.wordSize - 2}b}"
+                        run_type = string[0]
+                else:
+                    result += "1" + run_type + f"{run_len:0{self.wordSize - 2}b}"
+                    result += "0" + string
+                    run_type = None
+                    in_run = False
+                    run_len = 0
+
+            else:
+                if self.isRun(string):
+                    in_run = True
+                    run_len = 1
+                    run_type = string[0]
+                else:
+                    result += "0" + string
+
+        # print(result)
